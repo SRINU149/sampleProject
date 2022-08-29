67d3ca46e24feb21ffacc821c64e3ee69cf8fad6
SRINU149 <st2020java@gmail.com>
2022-08-29 18:29:39 +0530

    commited two files

---
+++ src/main/java/com/sreenutech/CitiConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.sreenutech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CitiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiConfigServerApplication.class, args);
+	}
+
+}
